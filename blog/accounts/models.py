06060a0cf3f4651e1805cd23761556c0ff46c6fa
sixdgt <edchristian.inventor@gmail.com>
2023-02-28 14:36:18 +0545

    Password reset API

--- blog/accounts/models.py
+++ blog/accounts/models.py
@@ -51,7 +51,7 @@ class Author(BaseModel):
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
-    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+    email_plaintext_message = "http://127.0.0.1{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
 
     send_mail(
         "Password Reset for {title}".format(title="requested user."),
